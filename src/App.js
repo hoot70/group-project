997ec716b015452a9d66ee2e2efa208e812713bb
Jon Osterhout <josterhout70@gmail.com>
2019-04-30 10:40:45 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo, { ReactComponent } from './logo.svg'
 import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import * as firebase from 'firebase';
 
-function App() {
+var config = {
+  apiKey: "AIzaSyAia2WZr0S88ovOJ7JWmCjRz63aoIiWpZw",
+  authDomain: "groupproject-45878.firebaseapp.com",
+  databaseURL: "https://groupproject-45878.firebaseio.com",
+  projectId: "groupproject-45878",
+  storageBucket: "groupproject-45878.appspot.com",
+  messagingSenderId: "281486642276"
+};
+
+firebase.initializeApp(config)
+const database = firebase.database()
+
+class App extends React.Component {
+  constructor (){
+    super()
+  };
+
+  render (){
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +40,7 @@ function App() {
       </header>
     </div>
   );
+}
 }
 
 export default App;
