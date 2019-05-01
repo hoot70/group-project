f48d748b6859b091633a441fcf9c4e40743abc88
Jon Osterhout <josterhout70@gmail.com>
2019-05-01 16:58:48 -0400

    'update'

--- src/components/enter.js
+++ src/components/enter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {NavLink } from "react-router-dom";
 
-const Enter = () => <div className="Enter"><NavLink to="/location">Submit</NavLink></div>
+const Enter = () => <div><NavLink to="/location">Submit</NavLink></div>
 
 export default Enter;
--- src/components/home.js
+++ src/components/home.js
@@ -1,17 +1,59 @@
 import React from 'react';
 import '../css/home.css';
-import Enter from "./enter"
+// import Enter from "./enter";
+import {BrowserRouter, Route, Switch, Link, withRouter } from 'react-router-dom';
+// import { , hashHistory,  browserHistory } from 'react-router'
+import * as firebase from 'firebase';
 
-const Home = () =>
-{
+var config = {
+    apiKey: "AIzaSyAia2WZr0S88ovOJ7JWmCjRz63aoIiWpZw",
+    authDomain: "groupproject-45878.firebaseapp.com",
+    databaseURL: "https://groupproject-45878.firebaseio.com",
+    projectId: "groupproject-45878",
+    storageBucket: "groupproject-45878.appspot.com",
+    messagingSenderId: "281486642276"
+  };
+  
+  firebase.initializeApp(config)
+  const database = firebase.database()
+
+class Home extends React.Component {
+    constructor (props){
+      super(props);
+       this.state = {
+        location: 'Where do You Want to Go'
+      };
+    };
+    componentDidMount(){
+        const textRef = database.ref('location/');
+        
+        textRef.on('value', snapshot => {
+          this.setState({
+            location: snapshot.val()
+          })
+        })
+      }
+    
+      writeData = e => {
+        e.preventDefault();
+        const locationValue = e.target.elements.inputLocation.value;
+        database.ref('location/').set(locationValue, function(error){
+          error ? alert('error') : console.log('Good Job!')})
+          this.props.history.push('/location');
+      }
+
+      render (){
     return(
      <div className="Homepage">
      <div>Enter Your Location
          <br />
-     <input type="text" name="inputText"/>
+         <form onSubmit={this.writeData.bind(this)}>
+     <input type="text" name="inputLocation" />
+     <button type='submit'>Submit</button>
+     </form>
      </div>
-     <Enter>Hello</Enter>
      </div>
     )
+      }
 }
-export default Home;
+export default withRouter(Home);
