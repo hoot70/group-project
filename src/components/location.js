53166db118be8d51d1f4ac6b65b21ce342bef01f
hoot70 <45696303+hoot70@users.noreply.github.com>
2019-05-02 08:44:02 -0400

    Update location.js

--- src/components/location.js
+++ src/components/location.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import '../css/location.css';
 import {NavLink} from 'react-router-dom'
+import * as firebase from 'firebase';
+import Home from './home'
 
-const location = () =>
-{
+const database = firebase.database()
+
+class Location extends React.Component {
+    constructor(){
+        super ();
+        this.state = {
+            location: ''
+          };
+    }
+    componentDidMount(){
+        const textRef = database.ref('location/');
+        
+        textRef.on('value', snapshot => {
+          this.setState({
+            location: snapshot.val()
+          })
+        })
+      }
+    render (){
     return(
      <div className="location">
+     <h1>Welcome to {this.state.location}</h1>
      <div>Enter The Cuisine You Are Looking For
          <br />
      <input type="text" name="inputText"/>
@@ -14,4 +34,5 @@ const location = () =>
      </div>
     )
 }
-export default location;
+}
+export default Location;
